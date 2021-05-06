b16057cfaea0c305afe75590029d7064bccf9561
zhiyu414 <83740867+zhiyu414@users.noreply.github.com>
2021-05-06 11:32:06 +0800

    Add files via upload

---
+++ DigitalReadSerial.ino
@@ -0,0 +1,28 @@
+
+
+// digital pin 2 has a pushbutton attached to it. Give it a name:
+int pushButton = 2;
+int ledPin =  13;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT_PULLUP);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  int buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+   if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin,HIGH );
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+  Serial.println(buttonState);
+  delay(1);        // delay in between reads for stability
+}
